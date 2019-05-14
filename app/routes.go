c7d2c84fe67618a1b4bd4d0884961bae3d4d9811
Nora Galvin <nora@gempages.help>
2019-05-14 15:18:58 +0700

    up

--- app/routes.go
+++ app/routes.go
@@ -11,16 +11,6 @@ func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	// router.Use(auth.ValidateAuth)
 
-	/* Theme router
-	--Default API router must be:
-	---/api/themes -> GET: get list themes
-	---/api/themes/{id} -> GET: get a theme by id
-	---/api/themes -> POST: store new theme
-	---/api/themes/{id} -> PUT: edit theme by id
-	---/api/themes/{id} -> DELETE: destroy theme by id
-	*/
-	// router.HandleFunc("/api/admin/theme", controllers.ThemeAdminUpdate).Methods("POST")
-
 	router.HandleFunc("/api/send-mail/{destination}", controllers.MailSend).Methods("GET")
 
 	return router
